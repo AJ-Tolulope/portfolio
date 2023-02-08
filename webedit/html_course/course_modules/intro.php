141585b1e53c0ce1765ce086cf89497b3d57e08a
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2023-02-08 03:05:59 +0100

    Add files via upload

---
+++ webedit/html_course/course_modules/intro.php
@@ -0,0 +1,24 @@
+<p class="portfolio-item-title">HTML - Introduction</p>
+<!-- portfolio item details start -->
+<div class="portfolio-item-details">
+    <div class="row">
+        <div class="description">
+            <h2>Course Info:</h2>
+            <h3>What Is HTML?</h3>
+            <p>HTML is an acronym- meaning <span style="color: var(--main-color);">H</span>yper<span style="color: var(--main-color);">T</span>ext <span style="color: var(--main-color);">M</span>arkup <span style="color: var(--main-color);">L</span>anguage - which is the standard markup language for creating a web page. First of all let's break things down.</p>
+            <P><span>HyperText -</span> An <b>HyperText</b> is basically text which contains links to other texts. Thus, the link available on a webpage is called Hypertext. You clicked a link which brought you to this page, then you have just clicked an <b>HyperText</b>. </p>
+            <p><span>Markup -</span> Simply means to "mark-up" a text document. An element like a text, image, video and so on, are "mark-up" with the help of tags which tell the browser how to structure them to display. Marking up a text makes the web page more interactive and dynamic.</p>
+            <p><span>And then languge -</span>  as other languages have their syntax, html also has it's own syntax, if you code it the wrong way you won't be communicating to the browser.</p>
+            <!-- for advance html --- <p><span>Web Page - </span> A web page is a document which is commonly written in HTML and translated by a web browser. We now know that html is not a programming language but a text file which will help display what we want on a browser. A web page can be identified by entering a URL. A Web page can be of the static or dynamic type. With the help of HTML only, we can create static web pages</p> -->
+            <p>The language all browers understand. It's basically a language used for almost all web pages. Html is the language that helps us structure our web page, consisting series of elements which will define the layout and the look of our website. Technically, HTML is a Markup language rather than a programming language.</p>
+            <p>Below is a simple html document.</p>
+            <img src="../images/codes/html_file.png" alt="code">
+            <p>What you see above is a simple html file with the head and body. If you noticed, the head and body are insde the main html tag. The three most important tags of an html file are the &lt;html&gt; tag which enclose the &lt;head&gt; and &lt;body&gt; tag. More about tags in the next module.</p>
+            <p>Complete the exercise to this module in the exercise section in order to move to the next module.</p>
+            <div class="note">
+                <p><span>NOTE</span> - HTML file is created by adding an html extension (.php) at the end of the file's name. Example index.html. "index.html" is a file refered to as the home page of a website. So when a website loads, the browser opens the index.html file first. Other files can be named differently.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- portfolio item details end -->
