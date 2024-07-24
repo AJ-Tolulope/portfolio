94d41acf41839ac6fd8ed4997ef3f2befbd4c1c4
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2024-07-24 03:39:37 +0100

    Update styles.css and main.js for smoother scrolling and animation effects

--- js/main.js
+++ js/main.js
@@ -4,28 +4,6 @@ AOS.init({
   once: true,
 });
 
-/* ------------------------- gsap anination start -------------------- */
-// gsap.registerPlugin(SplitText);
-
-// var tl = gsap.timeline(),
-//   mySplitText = new SplitText("#text", { type: "words,chars" }),
-//   chars = mySplitText.chars; //an array of all the divs that wrap each character
-
-// gsap.set("#text", { perspective: 400 });
-
-// console.log(chars);
-
-// tl.from(chars, {
-//   duration: 0.8,
-//   opacity: 0,
-//   scale: 0,
-//   y: 80,
-//   rotationX: 180,
-//   transformOrigin: "0% 50% -50",
-//   ease: "back",
-//   stagger: 0.01
-// });
-/* ------------------------- gsap anination end -------------------- */
 
 /* ------------------------- progress bar start -------------------- */
 window.addEventListener("scroll", () => {
